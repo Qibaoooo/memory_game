36a1bbef7b018cbefd676ea04061d7b7d52bfa51
Qibaoooo <luozhengiw@gmail.com>
2024-01-03 23:07:09 +0800

    added ImageDownloader

---
+++ app/src/main/java/nus/iss/sa57/team11/ImageDownloader.java
@@ -0,0 +1,74 @@
+package nus.iss.sa57.team11;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ImageDownloader {
+
+    protected boolean downloadAllImages(String imgURL, File dir) {
+        try {
+            Document doc = Jsoup.connect(imgURL).get();
+            List<Element> imgs = doc.getElementsByTag("img");
+            List<String> img_urls = imgs.stream()
+                    .map(e -> e.absUrl("src"))
+                    .filter(s -> s.endsWith("jpg"))
+                    .collect(Collectors.toList());
+
+            if (img_urls.size() < 20) {
+                Log.e("downloadImage", "The website has < 20 imgs shown. Choose another website.");
+            }
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 4; j++) {
+                    String url = img_urls.get(4 * i + j);
+                    File destFile = new File(dir, url);
+                    downloadImage(url, destFile);
+                }
+            }
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    protected boolean downloadImage(String imgURL, File destFile) {
+        try {
+            URL url = new URL(imgURL);
+            URLConnection conn = url.openConnection();
+
+            InputStream in = conn.getInputStream();
+            FileOutputStream out = new FileOutputStream(destFile);
+
+            byte[] buf = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = in.read(buf)) != -1) {
+                out.write(buf, 0, bytesRead);
+            }
+
+            out.close();
+            in.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
--- app/src/main/java/nus/iss/sa57/team11/MainActivity.java
+++ app/src/main/java/nus/iss/sa57/team11/MainActivity.java
@@ -3,14 +3,22 @@ package nus.iss.sa57.team11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     protected Button fetch_btn;
+    private final String DEFAULT_URL = "https://stocksnap.io/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         setupFetchButton();
+        setupImages();
     }
 
     @Override
@@ -30,6 +39,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void setupFetchButton() {
         fetch_btn = findViewById(R.id.fetch_btn);
         fetch_btn.setOnClickListener(this);
+    }
+
+    private void setupImages() {
+        EditText et = findViewById(R.id.edit_url);
+        et.setText(DEFAULT_URL);
+
+        TableLayout imgTable = findViewById(R.id.img_table);
+        for (int i = 0; i < 5; i++) {
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
+            for (int j = 0; j < 4; j++) {
+                ImageView holder = new ImageView(this);
+                holder.setImageResource(R.drawable.ic_launcher_background);
+                tr.addView(holder);
+            }
+            imgTable.addView(tr);
+        }
+
+        startDownloadImage(DEFAULT_URL);
+    }
+
+    protected void startDownloadImage(String imgURL) {
+        // creating a background thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TableLayout imgTable = findViewById(R.id.img_table);
+                ImageDownloader imgDL = new ImageDownloader();
+                File externalFilesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                if (imgDL.downloadAllImages(imgURL, externalFilesDir)) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            File[] files = externalFilesDir.listFiles();
+
+                            Log.d("debug", "here");
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 
 }
