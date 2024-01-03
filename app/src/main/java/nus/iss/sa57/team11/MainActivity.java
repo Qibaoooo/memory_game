e3746fe97c5a06235fe9c8d7a9e7e9b638f60df0
Qibaoooo <luozhengiw@gmail.com>
2024-01-03 21:31:14 +0800

    added fetch button UI

--- app/src/main/java/nus/iss/sa57/team11/MainActivity.java
+++ app/src/main/java/nus/iss/sa57/team11/MainActivity.java
@@ -3,12 +3,33 @@ package nus.iss.sa57.team11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected Button fetch_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupFetchButton();
     }
+
+    @Override
+    public void onClick(View v) {
+        EditText et = findViewById(R.id.edit_url);
+        String URLString = String.valueOf(et.getText());
+        Log.d("debug", URLString);
+    }
+
+    private void setupFetchButton() {
+        fetch_btn = findViewById(R.id.fetch_btn);
+        fetch_btn.setOnClickListener(this);
+    }
+
 }
