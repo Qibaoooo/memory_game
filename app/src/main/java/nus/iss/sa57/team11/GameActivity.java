b8f3543533939afd82832d25601f4167f0635dc4
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2024-01-05 17:54:17 +0800

    some modification

--- app/src/main/java/nus/iss/sa57/team11/GameActivity.java
+++ app/src/main/java/nus/iss/sa57/team11/GameActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TableLayout;
@@ -14,30 +15,41 @@ import android.widget.TableRow;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
     //private File externalFilesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    private boolean isFirstClick = true;
+    List<String> imgPaths;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-
-        setPictures(getImgFilesDir());
+        imgPaths = getImgFilesDir();
+        setImgHolders();
+        //setPictures();
     }
-    //going to work on game activity
 
-    private void setPictures(List<String> imgPath) {
+    @Override
+    public void onClick(View v){
+        if(isFirstClick){
+            int id = v.getId();
+            setPicture(imgPaths,id);
+        }
+    }
+
+    private void setImgHolders(){
         TableLayout imgTable = findViewById(R.id.game_img_table);
-        for (int i = 0; i < 3; i++) {
+        int index = 0;
+        for (int i = 0; i < 4; i++) {
             TableRow tr = new TableRow(this);
             tr.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
-            for (int j = 0; j < 2; j++) {
+            for (int j = 0; j < 3; j++) {
                 ImageView holder = new ImageView(this);
-                holder.setImageResource(R.drawable.ic_launcher_background);
+                holder.setImageResource(R.drawable.q_mark);
                 TableRow.LayoutParams params = new TableRow.LayoutParams(
                         TableRow.LayoutParams.WRAP_CONTENT,
                         TableRow.LayoutParams.WRAP_CONTENT,
@@ -48,14 +60,28 @@ public class GameActivity extends AppCompatActivity {
                 holder.getLayoutParams().height = 400; //can change the size according to you requirements
                 holder.requestLayout();
                 holder.setScaleType(ImageView.ScaleType.FIT_XY);
+                holder.setId(index++);
+                holder.setOnClickListener(this);
 
                 tr.addView(holder);
             }
             imgTable.addView(tr);
         }
+    }
+
+    private void setPicture(List<String> imgPath,int index){
+        TableLayout imgTable = findViewById(R.id.game_img_table);
+        TableRow tr = (TableRow) imgTable.getChildAt(index / 3);
+        ImageView iv = (ImageView) tr.getChildAt(index % 3);
+        int id = this.getResources().getIdentifier(imgPath.get(index),"drawable",this.getPackageName());
+        iv.setImageResource(id);
+    }
+
+    private void setPictures(List<String> imgPath) {
+        TableLayout imgTable = findViewById(R.id.game_img_table);
         int index = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
                 TableRow tr = (TableRow) imgTable.getChildAt(i);
                 ImageView iv = (ImageView) tr.getChildAt(j);
                 //Bitmap bitmap = BitmapFactory.decodeFile(destFile.getAbsolutePath());
@@ -72,6 +98,12 @@ public class GameActivity extends AppCompatActivity {
         //List<String> modifiedimgList = Arrays.stream(imgList)
         //        .map(e -> imgPath + e + ".png")
         //        .collect(Collectors.toList());
-        return Arrays.asList("t0", "t1", "t2", "t3", "t4", "t5");
+        List<String> originalName = Arrays.asList("t0", "t1", "t2", "t3", "t4", "t5");
+        List<String> res = new ArrayList<>();
+        res.addAll(originalName);
+        res.addAll(originalName);
+        Collections.shuffle(res);
+
+        return res;
     }
 }
